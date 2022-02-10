e72e4e1fce08a97faf12520d7f892276c9b729fd
Mayank Ashok <mayank@divertica.com>
2022-02-10 15:59:33 -0800

    Add fn: fetchClients and update ClientList table to display client data

--- src/components/ClientList.tsx
+++ src/components/ClientList.tsx
@@ -1,5 +1,4 @@
 import {
-  Grid,
   Paper,
   Table,
   TableBody,
@@ -9,48 +8,39 @@ import {
   TableRow,
 } from "@mui/material";
 
-const ClientList = () => {
-  function createData(
-    name: string,
-    calories: number,
-    fat: number,
-    carbs: number,
-    protein: number
-  ) {
-    return { name, calories, fat, carbs, protein };
-  }
+type ClientListProps = {
+  clientList: any[];
+};
 
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
+const ClientList = (props: ClientListProps) => {
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>ID</TableCell>
+            {props.clientList.length &&
+              Object.keys(props.clientList[0]).map((key, index) => {
+                return index !== 0 && index < 7 ? (
+                  <TableCell>{key}</TableCell>
+                ) : null;
+              })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {props.clientList.map((row: any, index) => (
             <TableRow
-              key={row.name}
+              key={row.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.id}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.first_name}</TableCell>
+              <TableCell align="right">{row.last_name}</TableCell>
+              <TableCell align="right">{row.email}</TableCell>
+              <TableCell align="right">{row.card_number}</TableCell>
+              <TableCell align="right">{row.balance}</TableCell>
+              <TableCell align="right">{row.isActive}</TableCell>
             </TableRow>
           ))}
         </TableBody>
