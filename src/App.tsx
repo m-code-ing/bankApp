2aa1f407e4c439550a5f515b35e2ba0312c2e59e
Mayank Ashok <mayank@divertica.com>
2022-02-10 14:44:24 -0800

    Add Component: add sample table

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { Button, Grid } from "@mui/material";
 import React from "react";
+import ClientList from "./components/ClientList";
 
 const App = () => {
-  return <div>React app</div>;
+  return (
+    <Grid
+      container
+      direction="column"
+      my={3}
+      justifyContent="center"
+      alignItems="center"
+      textAlign="center">
+      <Grid item my={6}>
+        <Button variant="contained">Get Clients</Button>
+      </Grid>
+      <Grid item>
+        <ClientList></ClientList>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default App;
