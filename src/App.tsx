7c7a4fab1d2703b496016bb9c5a8a4a4c8de0394
Mayank Ashok <mayank@divertica.com>
2022-02-10 16:33:14 -0800

    Add Component: add BankList component and Get Bank List button, connect same with api call

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import { Button, Grid } from "@mui/material";
 // Other imports
 import axios from "axios";
 import { useState } from "react";
+import BankList from "./components/BankList";
 
 const App = () => {
   const [clientList, setClientList] = useState<any[]>([]);
+  const [bankList, setBankList] = useState<any[]>([]);
 
   const fetchClients = () => {
     // GET client list
@@ -19,6 +21,17 @@ const App = () => {
     }).then(function (response) {
       const clientList = response.data;
       setClientList(clientList);
+    });
+  };
+
+  const fetchBankers = () => {
+    // GET client list
+    axios({
+      method: "get",
+      url: "http://localhost:8080/api/banks",
+    }).then(function (response) {
+      const bankList = response.data;
+      setBankList(bankList);
     });
   };
 
@@ -36,16 +49,30 @@ const App = () => {
       my={3}
       justifyContent="center"
       alignItems="center"
+      alignContent="center"
       textAlign="center">
-      <Grid item my={6}>
-        <Button variant="contained" onClick={() => fetchClients()}>
-          Get Clients
-        </Button>
+      <Grid container justifyContent="center">
+        <Grid item my={6} mx={2}>
+          <Button variant="contained" onClick={() => fetchClients()}>
+            Get Clients
+          </Button>
+        </Grid>
+        <Grid item my={6} mx={2}>
+          <Button variant="contained" onClick={() => fetchBankers()}>
+            Get Bankers
+          </Button>
+        </Grid>
       </Grid>
-      <Grid item>
-        <ClientList
-          clientList={clientList}
-          updateClientList={updateClientList}></ClientList>
+      <Grid sm={10} container justifyContent="center">
+        <Grid item my={2} border={1}>
+          <ClientList
+            clientList={clientList}
+            updateClientList={updateClientList}></ClientList>
+        </Grid>
+
+        <Grid item my={2} border={1}>
+          <BankList bankList={bankList}></BankList>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/components/BankList.tsx
+++ src/components/BankList.tsx
@@ -14,42 +14,32 @@ import DeleteIcon from "@mui/icons-material/Delete";
 
 import axios from "axios";
 
-type ClientListProps = {
-  clientList: any[];
-  updateClientList: (id: number) => void;
+type BankListProps = {
+  bankList: any[];
 };
 
-const ClientList = (props: ClientListProps) => {
-  const deleteClient = (id: number) => {
-    axios({
-      method: "DELETE",
-      url: `http://localhost:8080/api/client/${id}`,
-    }).then(function (response) {
-      props.updateClientList(id);
-    });
-  };
+const BankList = (props: BankListProps) => {
   return (
     <TableContainer component={Paper}>
       <Grid container justifyContent="center">
         <Grid sm={4} bgcolor="aliceblue" py={1} border={1}>
-          <Typography>Client List</Typography>
+          <Typography>Bank List</Typography>
         </Grid>
       </Grid>
-
       <Table sx={{ minWidth: 650 }} aria-label="simple table">
         <TableHead>
           <TableRow>
             <TableCell>ID</TableCell>
-            {props.clientList.length &&
-              Object.keys(props.clientList[0]).map((key, index) => {
-                return index !== 0 && index < 7 ? (
+            {props.bankList.length &&
+              Object.keys(props.bankList[0]).map((key, index) => {
+                return index !== 0 && index < 6 ? (
                   <TableCell>{key}</TableCell>
                 ) : null;
               })}
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.clientList.map((row: any, index) => (
+          {props.bankList.map((row: any, index) => (
             <TableRow
               key={row.id}
               sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
@@ -60,13 +50,9 @@ const ClientList = (props: ClientListProps) => {
               <TableCell align="right">{row.last_name}</TableCell>
               <TableCell align="right">{row.email}</TableCell>
               <TableCell align="right">{row.card_number}</TableCell>
-              <TableCell align="right">{row.balance}</TableCell>
-              <TableCell align="right">{row.isActive}</TableCell>
+              <TableCell align="right">{row.employee_number}</TableCell>
               <TableCell align="right">
-                <IconButton
-                  aria-label="delete"
-                  color="warning"
-                  onClick={() => deleteClient(row.id)}>
+                <IconButton aria-label="delete" color="warning">
                   <DeleteIcon />
                 </IconButton>
               </TableCell>
@@ -78,4 +64,4 @@ const ClientList = (props: ClientListProps) => {
   );
 };
 
-export default ClientList;
+export default BankList;
