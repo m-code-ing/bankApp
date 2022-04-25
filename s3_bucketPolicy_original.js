7466a80372f91c7cec76a3edbb676d0a4d723387
Mayank Ashok <mayank@divertica.com>
2022-04-24 21:08:02 -0700

    saving original s3 bucket policy for reference

---
+++ s3_bucketPolicy_original.js
@@ -0,0 +1,13 @@
+// Below is the Original Bucket policy on s3 bucket before editing/adding bucket policy to deploy static website
+// {
+// 	"Version": "2012-10-17",
+// 	"Statement": [
+// 		{
+// 			"Sid": "Statement1",
+// 			"Principal": {},
+// 			"Effect": "Allow",
+// 			"Action": [],
+// 			"Resource": []
+// 		}
+// 	]
+// }
